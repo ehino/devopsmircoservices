a11cd85b63c3c22c4154399e85562f588304e441
ehino <akhileehis@gmail.com>
2022-08-05 14:07:29 +0100

    deployment

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,18 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="ehisakhile/microservicesproject:v1.0"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run microservicesproject --image=ehisakhile/microservicesproject:v1.0 --port=80 --labels app=microservicesproject
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward microservicesproject 8000:80
 
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="ehisakhile/microservicesproject:v1.0"
 
 # Step 2:  
 # Authenticate & tag
+docker login
+docker tag microservicesproject $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
